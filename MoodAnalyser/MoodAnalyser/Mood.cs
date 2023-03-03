348f1d4bd42499264f5398e934c4733dca0dda76
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-03-03 09:06:16 +0530

    [Amol] : Adding for Analyse a Mood by using Constructor

--- MoodAnalyser/MoodAnalyser/Mood.cs
+++ MoodAnalyser/MoodAnalyser/Mood.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace MoodAnalyser
 {
     public class Mood
     {
+        string message;
+        public Mood(string message)
+        {
+            this.message = message;
+        }
         //for check Analyse Mood happy or sad
         //UC1 / UC2
-        public string AnalyseMood(string message)
+        public string AnalyseMood()
         {
             if (message.Contains("sad"))
             {
--- MoodAnalyser/TestMoodAnalyser/UnitTest1.cs
+++ MoodAnalyser/TestMoodAnalyser/UnitTest1.cs
@@ -4,18 +4,20 @@ namespace TestMoodAnalyser
 {
     public class Tests
     {
-        Mood checkMood = new Mood();
+        //Mood checkMood = new Mood();
                 
         [Test]
         public void SadMood_WhenAnalyse_ShouldReturnSad()
         {
-            string result = checkMood.AnalyseMood("I am in sad mood");
+            Mood checkMood = new Mood("I am in sad mood");
+            string result = checkMood.AnalyseMood();
             Assert.AreEqual(result, "sad");
         }
         [Test]
         public void HappyMood_WhenAnalyse_ShouldReturnSad()
         {
-            string result = checkMood.AnalyseMood("I am in happy mood");
+            Mood checkMood = new Mood("I am in happy mood");
+            string result = checkMood.AnalyseMood();
             Assert.AreEqual(result, "happy");
         }
     }
